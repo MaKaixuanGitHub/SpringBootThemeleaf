e0dd3e5bae5d7397445e3efc76267cf266690f8b
makaixuan <lanqiuxiaotong@outlook.com>
2022-08-11 16:38:00 +0800

    Add junit4 testClass

---
+++ src/test/java/com/kuang/controller/EmployeeControllerTest.java
@@ -0,0 +1,32 @@
+package com.kuang.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeControllerTest {
+
+    @Test
+    public void list() {
+    }
+
+    @Test
+    public void toAddpage() {
+    }
+
+    @Test
+    public void addpage() {
+    }
+
+    @Test
+    public void toUpdatePage() {
+    }
+
+    @Test
+    public void updateEmp() {
+    }
+
+    @Test
+    public void delEmp() {
+    }
+}
