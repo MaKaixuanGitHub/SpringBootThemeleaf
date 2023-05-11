d031da3d67ee50fe549e7a1782ba6c50e4b61dc5
makaixuan <lanqiuxiaotong@outlook.com>
2023-05-11 17:19:27 +0800

    20230511 makaixuan upd

--- src/main/java/com/kuang/Springboot03WebApplication.java
+++ src/main/java/com/kuang/Springboot03WebApplication.java
@@ -2,12 +2,10 @@ package com.kuang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.kuang.mapper"})
-@EnableOAuth2Sso
 public class Springboot03WebApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/kuang/TestJDBC.java
+++ src/test/java/com/kuang/TestJDBC.java
@@ -1,48 +1,12 @@
 package com.kuang;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 
 @SpringBootTest
 public class TestJDBC {
-//    //注入数据源
-//    @Autowired
-//    DataSource dataSource;
-//
-//    @Test
-//     public void contextLoads() throws SQLException {
-//        //查看默认的数据源
-//        System.out.println(dataSource.getClass());
-//        //获得数据库连接
-//        Connection connection = dataSource.getConnection();
-//        System.out.println(connection);
-//        //关闭连接
-//        connection.close();
-//    }
 
-    /***
-     * String Security Test
-     * @throws SQLException　SQLException
-     */
-    @Test
-     public void contextLoads() throws SQLException {
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        // 密码加密
-        String encode = passwordEncoder.encode("1234");
-
-        // 比较密码
-        boolean matches = passwordEncoder.matches("1234", encode);
-
-        System.out.println("encode=====>" + encode);
-        System.out.println("=====================");
-        System.out.println("matches=====>" + matches);
-    }
 }
